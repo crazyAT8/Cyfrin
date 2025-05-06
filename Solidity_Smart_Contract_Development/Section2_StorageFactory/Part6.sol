0d25eddbe52c16c9b00ff43a50eab13e2a19f973
crazyAT8 <88wwatson@gmail.com>
2025-05-05 21:46:42 -0500

    completed part6

--- Solidity_Smart_Contract_Development/Section2_StorageFactory/Part6.sol
+++ Solidity_Smart_Contract_Development/Section2_StorageFactory/Part6.sol
@@ -2,13 +2,29 @@
 
 pragma solidity ^0.8.18;
 
-import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
+import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
     SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        SimpleStorage newSimpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
         listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Address
+        // ABI - Application Binary Interface (technically a lie, you just need the function selector)
+        // SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        // mySimpleStorage.store(_newSimpleStorageNumber);
+        // to make things easier
+        return listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        // SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        // return mySimpleStorage.retrieve();
+        // to make things easier
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
