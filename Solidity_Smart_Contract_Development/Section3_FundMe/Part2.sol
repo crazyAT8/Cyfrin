926e56f6c13cdaf49af765454c701d42601fe77c
crazyAT8 <88wwatson@gmail.com>
2025-05-12 00:10:43 -0500

    Started section 3 parts 1&2

---
+++ Solidity_Smart_Contract_Development/Section3_FundMe/Part2.sol
@@ -0,0 +1,14 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract FundMe {
+    function fund() public {}
+
+    function withdraw() public {}
+
+}
