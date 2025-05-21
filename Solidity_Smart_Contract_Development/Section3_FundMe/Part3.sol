b2503410095e23dc45a7024e78218c14cabbfe3f
crazyAT8 <88wwatson@gmail.com>
2025-05-20 19:45:13 -0500

    part3

--- Solidity_Smart_Contract_Development/Section3_FundMe/Part3.sol
+++ Solidity_Smart_Contract_Development/Section3_FundMe/Part3.sol
@@ -7,7 +7,7 @@ contract FundMe {
         // Allow user to spend $
         // Have a minimum $ spent
         // 1. How do we send ETH to this contract?
-        require(msg.value > 1e18); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
+        require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
     }
 
     // function withdraw() public {}
