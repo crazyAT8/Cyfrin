cc85da317b020c99557dcd83e7b3c9e9839d62e0
crazyAT8 <88wwatson@gmail.com>
2025-06-11 21:24:43 -0500

    part 8 solidity interface

--- Solidity_Smart_Contract_Development/Section3_FundMe/Part8.sol
+++ Solidity_Smart_Contract_Development/Section3_FundMe/Part8.sol
@@ -2,6 +2,24 @@
 
 pragma solidity ^0.8.18; 
 
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe {
 
     uint256 public minimumUSD = 5; 
@@ -15,6 +33,13 @@ contract FundMe {
 
     // function withdraw() public {}
 
-    function getPrice() public {}
+    function getPrice() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+    }
     function getConversionRate() public {}
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 }
